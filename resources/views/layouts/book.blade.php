09c8e6a1a8aabd97f1ecafd2d08c45caf195c024
Vladimir547 <germes547@gmail.com>
2024-01-26 00:27:47 +0300

    fix updating user

--- resources/views/layouts/book.blade.php
+++ resources/views/layouts/book.blade.php
@@ -16,7 +16,7 @@
                                 @auth
                                     @if(auth()->user()->role == 'admin' or auth()->user()->id == $book->author->user_id)
                                         <div class="flex justify-center">
-                                            <a href="{{ asset("author/update/{$book->id}") }}" type="button"
+                                            <a href="{{ asset("book/update/{$book->id}") }}" type="button"
                                                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Изменить</a>
                                         </div>
                                     @endif
